0af050ba907a7fb954fd182456d0950dff268f3e
Wideka Ardhiguna <widekaardhiguna22@gmail.com>
2022-10-17 10:14:05 +0700

    mobile responsive MainLayout

--- layouts/MainLayout/MainLayout.tsx
+++ layouts/MainLayout/MainLayout.tsx
@@ -1,3 +1,4 @@
+import colorAlpha from "color-alpha"
 import { Drawer } from "components"
 import { useState } from "react"
 import { styled, theme } from "theme/config"
@@ -11,18 +12,36 @@ interface MainLayoutProps {
 const Wrapper = styled("div", {
   backgroundColor: "#404040",
   height: "100vh",
-  [`& > .container`]: {
+  "& > .container": {
     backgroundColor: theme.colors.background,
     height: "100%",
     overflow: "auto",
-    display: "grid",
-    gridTemplateColumns: "auto 1fr",
+    "& > .side": {
+      display: "none",
+    },
+    "& > .content": {
+      backgroundImage: "url('/bg_content.png')",
+      backgroundColor: colorAlpha(theme.colors.dark.value, 0.7),
+      backgroundBlendMode: "darken",
+      overflow: "auto",
+      height: "100%",
+    },
+  },
+
+  "@md": {
+    "& > .container": {
+      display: "grid",
+      gridTemplateColumns: "auto 1fr",
+      "& > .side": {
+        display: "block",
+      },
+    },
   },
 
   "@xl": {
     display: "grid",
     placeContent: "center",
-    [`& > .container`]: {
+    "& > .container": {
       width: 1400,
       height: "95vh",
       borderRadius: 8,
@@ -36,13 +55,13 @@ export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <Wrapper>
       <div className="container">
-        <div>
+        <div className="side">
           <Sidebar />
           <Drawer show={showDrawer}>
             <Sidebar />
           </Drawer>
         </div>
-        <Content>{children}</Content>
+        <main className="content">{children}</main>
       </div>
     </Wrapper>
   )
