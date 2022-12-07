da09a8046116f47e2358d6d08b091f24d8d94438
Wideka Ardhiguna <widekaardhiguna22@gmail.com>
2022-12-07 09:41:02 +0700

    Finished skills page

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -8,6 +8,19 @@ import cssLogo from "public/css-logo.png"
 import jsLogo from "public/js-logo.png"
 import tsLogo from "public/ts-logo.png"
 import nodeLogo from "public/node-logo.png"
+import reactLogo from "public/react-logo.png"
+import viteLogo from "public/vite-logo.svg"
+import zustandLogo from "public/zustand-logo.png"
+import muiLogo from "public/mui-logo.svg"
+import axiosLogo from "public/axios-logo.svg"
+import reactRouterLogo from "public/react-router-logo.svg"
+import reactQueryLogo from "public/react-query-logo.svg"
+import emotionLogo from "public/emotion-logo.png"
+import nextLogo from "public/next-logo.png"
+import expressLogo from "public/express-logo.png"
+import gitLogo from "public/git-logo.png"
+import githubLogo from "public/github-logo.png"
+import dockerLogo from "public/docker-logo.png"
 
 const Root = styled("div", {
   paddingTop: "6rem",
@@ -23,7 +36,8 @@ const Root = styled("div", {
     "& > .skill-section": {
       fontSize: "1.3rem",
       paddingLeft: "1rem",
-      marginBottom: "0.5rem",
+      marginTop: "0.8rem",
+      marginBottom: "0.8rem",
     },
   },
 })
@@ -31,6 +45,23 @@ const Root = styled("div", {
 const BodyWrapper = styled("div", {
   padding: "0.5rem 1rem",
   fontSize: "0.925rem",
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.4rem",
+})
+
+const List = styled("ul", {
+  listStyleType: "none",
+  margin: 0,
+  paddingLeft: "0.4rem",
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.7rem",
+  "& > li": {
+    display: "flex",
+    alignItems: "center",
+    gap: "0.6rem",
+  },
 })
 
 const Home: NextPageWithLayout = () => {
@@ -123,6 +154,142 @@ const Home: NextPageWithLayout = () => {
                 I usually need to build a proxy if the backend application use
                 microservice as their architecure. If the backend app is
                 monolithic, most of the time a proxy is not needed to built .
+              </Text>
+            </BodyWrapper>
+          }
+        />
+      </section>
+      <section className="skills">
+        <Text as="h2" className="skill-section">
+          Frameworks
+        </Text>
+        <Collapsed
+          title="React"
+          icon={<Image src={reactLogo} alt="react logo" height="24" />}
+          body={
+            <BodyWrapper>
+              <Text>
+                Probably my &quot;bread and butter&quot; skill when building web
+                apps. A light library that suitable for small to complex
+                application. Nowadays there are a lot of alternative that might
+                seems easier than React, but I still think React is still the
+                best in term of flexibility and it&apos;s huge ecosystem.
+              </Text>
+              <Text>
+                React alone usually is not enough to build web apps, because
+                it&apos;s just a small library by it self. Here are some of
+                tools I usually use to support my React app :
+              </Text>
+              <List>
+                <li>
+                  <Image src={viteLogo} alt="vite logo" height="18" />
+                  Vite
+                </li>
+                <li>
+                  <Image
+                    src={reactRouterLogo}
+                    alt="react router logo"
+                    height="18"
+                    width="18"
+                  />
+                  React Router
+                </li>
+                <li>
+                  <Image
+                    src={reactQueryLogo}
+                    alt="react query logo"
+                    height="18"
+                    width="18"
+                  />
+                  React Query
+                </li>
+                <li>
+                  <Image
+                    src={zustandLogo}
+                    alt="zustand logo"
+                    height="18"
+                    width="18"
+                  />
+                  Zustand
+                </li>
+                <li>
+                  <Image src={emotionLogo} alt="emotion logo" height="18" />
+                  Emotion
+                </li>
+                <li>
+                  <Image src={muiLogo} alt="material ui logo" height="18" />
+                  Material UI
+                </li>
+              </List>
+            </BodyWrapper>
+          }
+        />
+        <Collapsed
+          title="Next JS"
+          icon={<Image src={nextLogo} alt="next js logo" height="24" />}
+          body={
+            <BodyWrapper>
+              <Text>
+                A React Framework, suitable to build a more performant web apps
+                because it&apos;s ability to SSR and CSR. Arguably has better
+                developer experience than React, but less flexible because how
+                opinionated it is. I use this instead of Vanilla React if better
+                SEO is required for the website.
+              </Text>
+            </BodyWrapper>
+          }
+        />
+        <Collapsed
+          title="Express"
+          icon={<Image src={expressLogo} alt="express logo" height="24" />}
+          body={
+            <BodyWrapper>
+              <Text>
+                Node JS Framework for more easier routing, middleware, error
+                handling, etc. I usually use this to help building a proxy.
+              </Text>
+            </BodyWrapper>
+          }
+        />
+      </section>
+      <section className="skills">
+        <Text as="h2" className="skill-section">
+          Others
+        </Text>
+        <Collapsed
+          title="Git"
+          icon={<Image src={gitLogo} alt="git logo" height="24" />}
+          body={
+            <BodyWrapper>
+              <Text>
+                A Tool used for versioning code. I use this extensively to
+                manage the version of softwares that I build.
+              </Text>
+            </BodyWrapper>
+          }
+        />
+        <Collapsed
+          title="Github"
+          icon={<Image src={githubLogo} alt="github logo" height="24" />}
+          body={
+            <BodyWrapper>
+              <Text>
+                Github is a platform for collaboration between developers.
+                Github work hand in hand with Git. Working together as a team
+                will be much easier with help from Github.
+              </Text>
+            </BodyWrapper>
+          }
+        />
+        <Collapsed
+          title="Docker"
+          icon={<Image src={dockerLogo} alt="docker logo" height="24" />}
+          body={
+            <BodyWrapper>
+              <Text>
+                Docker is an open source platform for building, deploying, and
+                managing containerized applications. I use docker to make my
+                application more portable and easier to deploy.
               </Text>
             </BodyWrapper>
           }
