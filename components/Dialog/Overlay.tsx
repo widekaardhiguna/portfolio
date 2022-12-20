ad8b3cedf41a863af2c4d6e3fbd7af21c827f402
Wideka Ardhiguna <widekaardhiguna22@gmail.com>
2022-12-20 19:36:10 +0700

    darken overlay dialog

--- components/Dialog/Overlay.tsx
+++ components/Dialog/Overlay.tsx
@@ -3,7 +3,7 @@ import colorAlpha from "color-alpha"
 import { styled, theme } from "theme/config"
 
 export const Overlay = styled(Dialog.Overlay, {
-  backgroundColor: colorAlpha(theme.colors.dark.value, 0.4),
+  backgroundColor: colorAlpha(theme.colors.dark.value, 0.7),
   position: "fixed",
   inset: 0,
   // animation: `${overlayShow} 150ms cubic-bezier(0.16, 1, 0.3, 1)`,
