45d32dbf93a1d00cd928452b5b7f668965e514a0
Wideka Ardhiguna <widekaardhiguna22@gmail.com>
2022-12-21 09:45:49 +0700

    Added meta og:image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,8 @@ export default function Document() {
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
+        <meta property="og:title" content="Wideka Ardhiguna Portfolio" />
+        <meta property="og:image" content="/thumbnail.png" />
       </Head>
       <body>
         <Main />
